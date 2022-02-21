428b0ce477cf15663c147aeed7cf9c8b6a9f3a12
jmosqueras <javiermosquerasanchez@gmail.com>
2022-02-21 13:26:04 +0100

    Formating fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "rgname" {
-    description = "Resource Group Name"
-    default     = "rgdemo"
-    type        = string
+  description = "Resource Group Name"
+  default     = "rgdemo"
+  type        = string
 }
 variable "location" {
-    description = "Azure location"
-    default     = "westeurope"
-    type        = string  
+  description = "Azure location"
+  default     = "westeurope"
+  type        = string
 }
 variable "sname" {
   description = "Azure Storage Account"
